89d96e4480dc3d7182f8514e1ce20c250309844c
Frederik Mbius Rygaard <fmry@pop-os.localdomain>
2026-01-21 15:45:31 +0100

    Update

--- submit_runtime.py
+++ submit_runtime.py
@@ -85,31 +85,22 @@ def loop_jobs(wait_time = 1.0):
             "Pareto": [[2],1e-4],
             }
     
-    for lam in lams:
-        for T in Ts:
-            for man, vals in runs.items():
-                dims, tol = vals[0], vals[1]
-                for d in dims:
-                    for m in methods:
-                        time.sleep(wait_time+np.abs(np.random.normal(0.0,1.,1)[0]))
-                        generate_job(man, d, T, m, tol, lam)
-                        try:
-                            submit_job()
-                        except:
-                            time.sleep(100.0+np.abs(np.random.normal(0.0,1.,1)))
-                            try:
-                                submit_job()
-                            except:
-                                print(f"Job script with {lam}, {T}, {man}, {m}, {d}, {tol} failed!")
+    #loop_over_jobs(lams, Ts, runs, methods, wait_time)
 
     Ts = [100]
-    lams = [0.0, 5.0, 10.0, 20.0, 50.0, 100.0]
+    lams = [5.0, 10.0, 20.0, 50.0, 100.0]
     methods = ["sgd", "rmsprop_momentum", "rmsprop", "adamax", "adam", "adagrad"]
     methods = methods + ["ProbGEORCE_LS", "ProbGEORCE_Adaptive"]
     #sphere
-    runs = {"Sphere": [[1000],1e-4],
+    runs = {"Sphere": [[100],1e-4],
             }
+    
+    loop_over_jobs(lams, Ts, runs, methods, wait_time)
+                                
+#%% Submit call
 
+def loop_over_jobs(lams, Ts, runs, methods, wait_time):
+    
     for lam in lams:
         for T in Ts:
             for man, vals in runs.items():
@@ -126,6 +117,8 @@ def loop_jobs(wait_time = 1.0):
                                 submit_job()
                             except:
                                 print(f"Job script with {lam}, {T}, {man}, {m}, {d}, {tol} failed!")
+                                
+    return
 
 #%% main
 
